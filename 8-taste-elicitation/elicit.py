7d0f57804e99d38139f67e639c14fe961edbb8ea
chocokookie <whgudwlsdlrm@naver.com>
2024-10-11 09:11:46 +0900

    add chainlit

--- 8-taste-elicitation/elicit.py
+++ 8-taste-elicitation/elicit.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import re
+import json
 
 import openai
 
@@ -7,8 +9,8 @@ from langchain import LLMChain, PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 
-import re
-import json
+import chainlit as cl
+
 
 
 ## Load API
@@ -21,9 +23,15 @@ manager = ChatOpenAI(
     temperature = 0
 )
 
+reply_bot = ChatOpenAI(
+    model = 'gpt-4o-mini',
+    temperature = 0
+)
+
 
 ## 0) subtask manager
 
+# 기본적인 소통을 위한 페르소나 설정도 들어가있으면 좋을 듯
 hj_subtask = '''The AI assistant should analyze the dialogue context to detect which sub-task should be selected.
 
 Sub-task name: It provides a unique identifier for sub-task detection, which is used for references to related expert models and their predicted results.
@@ -35,6 +43,9 @@ user_query: I want to find a legal drama
 answer: user preference elicitation
 
 user_query: I want the female actor as the lead role
+answer: user preference elicitation
+
+user_query: I can't wait anymore. Give me reccomendations
 answer: recommendation
 
 user_query: Why do you recommend it to me?
@@ -115,12 +126,12 @@ def taste_elicit(query):
     )
 
     # Chain 정의
-    chain = LLMChain(
+    subtask_chain = LLMChain(
         llm = manager,
         prompt = taste_elicitation
     )
 
-    result = chain.invoke({'query': query}) # chain 실행
+    result = subtask_chain.invoke({'query': query}) # chain 실행
 
     pattern = r'^```json\s*|\s*```$'
     cleaned_text = re.sub(pattern, '', result['text'], flags=re.DOTALL).strip()
@@ -132,6 +143,87 @@ user_profile = {
     'movie_title': [],
     'director': [],
     'actor': [],
-    'requirements': [],
-    'genre': []
+    'genre': [],
+    'requirements': []
 }
+
+
+## 2) Reply Agent
+
+reply_message = """This is an movie recommendation task and your assignment is to interact and ask user's taste. Follow several rules below.
+
+1) Treat user as if you are an counselor.
+2) AI assitant should ask questions to fill the user profile dictionary.
+3) There are 4 necessary items and 1 optional item.
+    neccesary items: [movie_title, director, actor, genre]
+    optional item: requirements
+4) Speak Korean
+
+[user profile]
+movie_title: {movie_title}
+director: {director}
+actor: {actor}
+genre: {genre}
+
+your question:
+"""
+
+def reply(user_profile):
+    '''
+    reply module
+    input
+        user_profile: 현재까지 elicit한 결과물
+    '''
+    reply_prompt = PromptTemplate(
+        template  = reply_message,
+        input_variables = ['movie_title','director','actor','genre']
+    )
+
+    reply_chain = LLMChain(
+        llm = reply_bot,
+        prompt = reply_prompt
+    )
+
+    result = reply_chain.invoke({'movie_title': user_profile['movie_title'], 'director': user_profile['director'], 'actor': user_profile['actor'], 'genre': user_profile['genre']})
+    return result['text']
+
+## 3) Chainlit
+Q = {}
+Q['start'] = 'Welcome to SmallTalk Lab! \nPlease let me know about your "movie tastes"!'
+Q['movie_title'] = 'What is your favorite movie?'
+Q['director'] = 'Who is your favorite director?'
+Q['actor'] = 'Who is your favorite actors/actresses'
+Q['genre'] = 'What is your favorite genre?'
+Q['requirements'] = 'Give me any requirements'
+
+
+
+@cl.on_chat_start
+async def on_chat_start():    
+    await cl.Message(content = Q['start']).send()
+
+
+@cl.on_message
+async def on_message(message):
+
+    utterance = message.content
+    subtask_type = subtask_detect(utterance)
+    print(subtask_type)
+
+    # 이 부분 훨씬 고급지게 callback 문으로 처리할 수 있을 것 같은데
+    if 'user preference elicitation' in subtask_type.lower():
+        taste_temp = taste_elicit(utterance)
+        print(taste_temp)
+        user_profile[taste_temp['content']].append((taste_temp['item'], taste_temp['preference_score']))
+    
+    # Reply Agent 대체 반복문
+    # for subject in user_profile:
+    #     if not user_profile[subject]:
+    #         await cl.Message(content = Q[subject]).send()
+    #         break
+    
+
+    # Reply Agent
+    reply2user = reply(user_profile)
+    print(user_profile)
+    await cl.Message(content = reply2user).send()
